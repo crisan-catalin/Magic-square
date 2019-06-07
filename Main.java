c2d943426d6f48d6630000baaac7a91537f970d3
Catalin Crisan <crisan.gheorghecatalin@gmail.com>
2019-06-07 18:33:38 +0300

    Create main method

--- Main.java
+++ Main.java
@@ -2,8 +2,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MagicSquare magicSquare = new MagicSquare(8,8);
-        magicSquare.generate();
-        magicSquare.display();
+        try {
+            System.out.println("MAGIC SQUARE\n\n");
+            MagicSquare magicSquare = new MagicSquare(8, 8, 0);
+            magicSquare.generate();
+
+            System.out.println("DECIMAL MATRIX");
+            magicSquare.displayDecimal();
+
+            System.out.println("BINARY MATRIX");
+            magicSquare.displayBinary();
+
+            System.out.println("MAGIC CUBE\n\n");
+            for (int i = 0; i < 64; i += 16) {
+                magicSquare = new MagicSquare(4, 4, i);
+                magicSquare.generate();
+
+                System.out.println("Cube");
+                magicSquare.displayDecimal();
+                System.out.println("\n");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
