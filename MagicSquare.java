c69018cc617ddb9c9a177def014f2ef417ef1f62
Catalin Crisan <crisan.gheorghecatalin@gmail.com>
2019-06-07 18:20:29 +0300

    Write a generic solution

--- MagicSquare.java
+++ MagicSquare.java
@@ -1,8 +1,14 @@
 public class MagicSquare {
     private int arr[][];
+    private String arrString[][];
 
-    public MagicSquare(int rows, int cols) {
+    public MagicSquare(int rows, int cols) throws Exception {
+        if (rows != cols || rows % 2 != 0) {
+            throw new Exception("MagicSquare support only square matrix with even number of rows/cols.");
+        }
+
         arr = new int[rows][cols];
+        arrString = new String[rows][cols];
     }
 
     public void generate() {
@@ -13,15 +19,18 @@ public class MagicSquare {
             for (int j = size - 1; j >= 0; j--) {
                 if (validateDiamondPattern(i, j)) {
                     arr[i][j] = currentValue;
+                    arrString[i][j] = getBinaryValue(currentValue, size - 1);
                 } else {
-                    arr[i][j] = maxValue - currentValue;
+                    int value = maxValue - currentValue;
+                    arr[i][j] = value;
+                    arrString[i][j] = getBinaryValue(value, size - 1);
                 }
                 currentValue++;
             }
         }
     }
 
-    public void display() {
+    public void displayDecimal() {
         int size = arr.length;
 
         for (int i = 0; i < size; i++) {
@@ -32,7 +41,23 @@ public class MagicSquare {
         }
     }
 
+    public void displayBinary() {
+        int size = arr.length;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(arrString[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private String getBinaryValue(int value, int digits) {
+        return String.format("%" + digits + "s", Integer.toBinaryString(value)).replace(' ', '0');
+    }
+
     private boolean validateDiamondPattern(int row, int col) {
+        int fullSize = arr.length;
         int size = arr.length - 1;
         int halfSize = size / 2;
 
@@ -42,15 +67,15 @@ public class MagicSquare {
 
         if (row <= halfSize) {
             if (col <= halfSize) {
-                return col + row == 3;
+                return col + row == Math.floor(size / 2);
             } else {
-                return col - row == 4;
+                return col - row == fullSize / 2;
             }
         } else {
             if (col <= halfSize) {
-                return row - col == 4;
+                return row - col == fullSize / 2;
             } else {
-                return col + row == 11;
+                return col + row == (Math.floor(size / 2) + 2 * fullSize / 2);
             }
         }
     }
